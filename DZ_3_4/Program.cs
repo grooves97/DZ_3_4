108f39f8d30dddba32a481011b7112ead2d41db4
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-01-03 20:51:48 +0600

    third HM

---
+++ DZ_3_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4.	Даны целые положительные числа A и B (A < B). Вывести все целые числа от A до B включительно;
+             * каждое число должно выводиться на новой строке;
+             * при этом каждое число должно выводиться количество раз, равное его значению. */
+
+            Console.WriteLine("Необходимо ввести A и B, причем A<B ");
+            try
+            {
+                Console.WriteLine("Введите A");
+                int A = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите B");
+                int B = Convert.ToInt32(Console.ReadLine());
+                if(A>=B || A<=0 || B <= 0)
+                {
+                    Console.WriteLine("Не соблюдено условие A<B либо числа A и B не положительные ");
+                    return;
+                }
+                Console.Clear();
+
+                for (int i = A; i < B; i++)
+                {
+                    for(int j = 1; j <= i; j++)
+                    {
+                        Console.Write(i);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Завершение программы ");
+                Environment.Exit(0);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZ_3_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DZ_3_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DZ_3_4")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("97d2c549-1fcc-49a6-9d3f-5a267ca505ab")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
